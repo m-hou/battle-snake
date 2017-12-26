f1f7a16d32678c6e50068d58c9ee6e7ffa7cc785
Michael Hou <michaeldamush@gmail.com>
2017-12-25 21:26:26 -0800

    Add number of reachable squares to evaluation. (#7)
    * Add number of reachable squares to evaluation.
    
    * Do not count other snake heads but count own in flood fill.
    
    * Use numpy array to add tuple.
    
    * Use number of squares_occupied in number of reachable_squares calculation so that the amount is constant after eatting food.

--- snakeai/snake_ai.py
+++ snakeai/snake_ai.py
@@ -1,5 +1,7 @@
 from collections import namedtuple
 from snakemodel.snake import Move
+from snakemodel.board import EntityId
+import numpy as np
 
 
 class SnakeAIv1():
@@ -11,7 +13,7 @@ class SnakeAIv1():
 
     DISCOUNT_FACTOR = 0.9
     DEPTH = 1
-    LARGE_PENALTY = -1000000000000
+    LARGE_PENALTY = np.array([-1000000000000, 0])
     FOOD_SCORE = 100
 
     def __init__(self, game):
@@ -43,9 +45,11 @@ class SnakeAIv1():
                     snake_id,
                     self.game.simulate_moves(board, {snake_id: possible_move}),
                     depth)
-                )
+            )
             for possible_move in Move]
-        return max(move_evals, key=lambda move_eval: move_eval.evaluation)
+        return max(
+            move_evals,
+            key=lambda move_eval: tuple(move_eval.evaluation))
 
     def _get_move_eval(self, snake_id, board, depth):
         """
@@ -56,7 +60,7 @@ class SnakeAIv1():
         Assume other snakes make the best immediate move.
         """
         board_eval = self._heuristic(snake_id, board)
-        if board_eval == self.LARGE_PENALTY:
+        if np.array_equal(board_eval, self.LARGE_PENALTY):
             return self.LARGE_PENALTY
         if depth <= 1:
             return board_eval
@@ -78,13 +82,31 @@ class SnakeAIv1():
                         for dead_snake_id, _ in board.dead_snakes.items()]:
             return self.LARGE_PENALTY
 
-        evaluation = 0
-        snake_head = board.snakes[snake_id].head
-        for food in board.food:
-            evaluation += self.FOOD_SCORE / (snake_head.distance(food)**2 + 1)
-        evaluation += len(board.snakes[snake_id].body) * self.FOOD_SCORE
-        return evaluation
+        snake = board.snakes[snake_id]
 
-    def _flood_fill(self, board):
-        """stub"""
-        raise NotImplementedError
+        reachable_squares = self._flood_fill(board, snake.head)
+        squares_occupied = len(set(snake.body))
+
+        evaluation = 0
+        for food in board.food:
+            evaluation += self.FOOD_SCORE / (snake.head.distance(food)**2 + 1)
+        evaluation += len(board.snakes[snake_id].body) * self.FOOD_SCORE
+        return np.array([reachable_squares + squares_occupied, evaluation])
+
+    def _flood_fill(self, board, cell):
+        """Count of safe squares that can be reached from cell."""
+        visited = set()
+        count = 0
+        to_visit = [cell]
+        while to_visit:
+            curr_cell = to_visit.pop()
+            if (curr_cell not in visited
+                    and board.cell_within_bounds(curr_cell)
+                    and (board.get_entity_at_cell(curr_cell) in
+                         [EntityId.EMPTY, EntityId.FOOD]
+                         or curr_cell == cell)):
+                count += 1
+                for move in Move:
+                    to_visit.append(move.apply_move_to_cell(curr_cell))
+            visited.add(curr_cell)
+        return count
