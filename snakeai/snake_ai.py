ac4232c76ad91467074d18dc204c082384842b10
m-hou <michaeldamush@gmail.com>
2017-12-28 16:38:58 -0800

    Add candidate move calculation to find moves that will not lead to death on the next move.

--- snakeai/snake_ai.py
+++ snakeai/snake_ai.py
@@ -35,13 +35,15 @@ class SnakeAI():
         MoveEvaluation = namedtuple("MoveEvaluation", "move, evaluation")
         move_evals = [
             MoveEvaluation(
-                move=possible_move,
+                move=candidate_move,
                 evaluation=self._get_move_eval(
                     snake_id,
-                    self.game.simulate_moves(board, {snake_id: possible_move}),
+                    self.game.simulate_moves(
+                        board,
+                        {snake_id: candidate_move}),
                     depth)
             )
-            for possible_move in Move]
+            for candidate_move in self._get_candidate_moves(snake_id, board)]
         return max(
             move_evals,
             key=lambda move_eval: tuple(move_eval.evaluation))
@@ -67,3 +69,34 @@ class SnakeAI():
         return board_eval + self.heuristic.DISCOUNT_FACTOR * \
             self._best_move_helper(
                 self.game.you, next_board, depth - 1).evaluation
+
+    def _get_candidate_moves(self, snake_id, board):
+        """Get moves that will not lead to immediate death."""
+        my_snake = board.snakes[snake_id]
+        _, possible_head_cells = self._get_snake_next_possible_cells(
+            self.game.you,
+            board)
+        candidate_moves = [move for move in Move]
+        for move, head_cell in possible_head_cells.items():
+            for other_snake_id in self.game.get_other_snake_ids(snake_id):
+                other_body_cell, other_possible_heads = \
+                    self._get_snake_next_possible_cells(
+                        other_snake_id,
+                        board)
+                other_snake = board.snakes[other_snake_id]
+                if head_cell in other_body_cell or (
+                        (head_cell in other_possible_heads and
+                         len(other_snake) >= len(my_snake))):
+                    candidate_moves.remove(move)
+                    break
+        return candidate_moves
+
+    def _get_snake_next_possible_cells(self, snake_id, board):
+        """Get possible cells that a snake can occupy on the next turn."""
+        snake = board.snakes[snake_id]
+        next_body_cells = set(snake.body[:-1])
+        possible_head_cells = {move: head_cell
+                               for move, head_cell in
+                               snake.get_possible_moves().items()
+                               if head_cell in next_body_cells}
+        return next_body_cells, possible_head_cells
