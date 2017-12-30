eb79043cba91efca504cb1b1116790b5b367b19e
m-hou <michaeldamush@gmail.com>
2017-12-29 21:21:28 -0800

    Add logging for game state data and best move.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 import json
+import logging
 from snakeai.heuristic import Heuristic
 from snakeai.snake_ai import SnakeAI
 from snakemodel.game import Game
+from logging.handlers import RotatingFileHandler
 from flask import Flask, request, jsonify
 
 
@@ -13,7 +15,6 @@ app = Flask(__name__)
 def start():
     # NOTE: 'request' contains the data which was sent to us about the Snake
     # game after every POST to our server
-    app.logger.info(request.__dict__)
     snake = {
         "color": "#ffffff",
         "name": "My Snake's Name!"
@@ -24,21 +25,24 @@ def start():
 
 @app.route("/move", methods=["POST"])
 def move():
-
     data = json.loads(request.data.decode("utf-8"))
     game = Game(data)
     heuristic = Heuristic()
     snake_ai = SnakeAI(game, heuristic)
     best_move = snake_ai.best_move()
 
+    app.logger.debug("{} \n {}".format(data, best_move))
     response = {
         "move": best_move
     }
-    app.logger.debug(best_move)
     return jsonify(response)
 
 
 if __name__ == "__main__":
     print("Starting server...")
     port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+    debug = bool(os.environ.get("DEBUG", True))
+    handler = RotatingFileHandler("debug.log")
+    handler.setLevel(logging.DEBUG)
+    app.logger.addHandler(handler)
+    app.run(host='0.0.0.0', port=port, debug=debug)
