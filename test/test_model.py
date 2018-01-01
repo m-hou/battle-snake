fe32634d0172cce592ea40013c545050692ea56e
m-hou <michaeldamush@gmail.com>
2018-01-01 12:44:38 -0800

    Clean code.

--- test/test_model.py
+++ test/test_model.py
@@ -10,23 +10,24 @@ dir = os.path.dirname(__file__)
 class TestBoardTransition(unittest.TestCase):
     """Tests for the simulating board transitions."""
 
-    def test_basic(self):
+    def test_simple_move(self):
         """Happy flow test."""
-        basic_file = os.path.join(dir, 'test_cases/basic.json')
-        basic_next_file = os.path.join(dir, 'test_cases/basic_next.json')
-        with open(basic_file, "r") as (
-            basic), open(basic_next_file, 'r') as (
-                basic_next):
-            basic_game = Game(json.load(basic))
-            basic_next_game = Game(json.load(basic_next))
-            my_snake_id = basic_game.you
+        simple_move_file = os.path.join(dir, 'test_cases/simple_move.json')
+        simple_move_next_file = os.path.join(
+            dir,
+            'test_cases/simple_move_next.json')
+        with open(simple_move_file, "r") as simple_move, open(
+                simple_move_next_file, 'r') as simple_move_next:
+            simple_move_game = Game(json.load(simple_move))
+            simple_move_next_game = Game(json.load(simple_move_next))
+            my_snake_id = simple_move_game.you
             self.assertEqual(
-                basic_game.simulate_moves(
-                    basic_game.board,
+                simple_move_game.simulate_moves(
+                    simple_move_game.board,
                     {my_snake_id:
                      Move.RIGHT,
-                     basic_next_game.get_other_snake_ids(my_snake_id)[0]:
-                     Move.DOWN}), basic_next_game.board)
+                     simple_move_next_game.get_other_snake_ids(my_snake_id)[0]:
+                     Move.DOWN}), simple_move_next_game.board)
 
     def test_collision(self):
         """Test collision between heads."""
@@ -34,9 +35,8 @@ class TestBoardTransition(unittest.TestCase):
         collision_next_file = os.path.join(
             dir,
             'test_cases/collision_next.json')
-        with open(collision_file, "r") as (
-            collision), open(collision_next_file, 'r') as (
-                collision_next):
+        with open(collision_file, "r") as collision, open(
+                collision_next_file, 'r') as collision_next:
             collision_game = Game(json.load(collision))
             collision_next_game = Game(json.load(collision_next))
             my_snake_id = collision_game.you
