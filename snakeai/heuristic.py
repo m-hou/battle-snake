8536a23f114f4a9f37a1674f96822c82404c7f7d
m-hou <michaeldamush@gmail.com>
2018-03-01 17:57:28 -0800

    Modularize heuristic.

--- snakeai/heuristic.py
+++ snakeai/heuristic.py
@@ -31,34 +31,38 @@ class Heuristic():
 
         snake = board.snakes[snake_id]
 
+        return np.array(
+            [self._get_tail_dist_penalty(snake, board),
+             self._get_open_squares(board, snake),
+             self._get_food_score(board, snake)])
+
+    def _get_tail_dist_penalty(self, snake, board):
+        max_dist_to_tail = self._get_max_dist_from_tail(snake, board)
+        travel_distance = get_travel_distance(
+                board, snake.head, snake.body[-1])
+        tail_dist_penalty = -travel_distance if (
+            travel_distance == CANT_FIND) else (
+            min(0, max_dist_to_tail - travel_distance))
+        return tail_dist_penalty
+
+    def _get_open_squares(self, board, snake):
         reachable_squares = safe_square_count(board, snake.head)
         squares_occupied = len(snake.body)
+        return reachable_squares + squares_occupied
 
+    def _get_food_score(self, board, snake):
         food_evaluation = CANT_FIND
         for food in board.food:
             food_evaluation = min(food_evaluation, food.distance(snake.head))
-        length_score = len(board.snakes[snake_id].body) * self.FOOD_SCORE
+        length_score = len(snake.body) * self.FOOD_SCORE
+        return -food_evaluation + length_score
 
-        max_dist_to_tail = self._get_max_dist_from_tail(
-            snake, board, food_evaluation)
-        travel_distance = get_travel_distance(
-                board, snake.head, snake.body[-1])
-        dist_from_tail_penalty = -travel_distance if (
-            travel_distance == CANT_FIND) else (
-            min(0, max_dist_to_tail - travel_distance))
-
-        return np.array(
-            [dist_from_tail_penalty,
-             reachable_squares + squares_occupied,
-             -food_evaluation + length_score])
-
-    def _get_max_dist_from_tail(self, snake, board, food_evaluation):
+    def _get_max_dist_from_tail(self, snake, board):
         """Threshold where there is no penalty for being too far from tail."""
 
         if snake.health_points == self.MAX_HEALTH:
+            # this is required or else snake will never eat
             return 1000
-        elif food_evaluation == CANT_FIND:
-            return 2
         else:
             return 2 + (self.MAX_HEALTH - snake.health_points) / (
                 self.MAX_HEALTH) * (board.width + board.height)
